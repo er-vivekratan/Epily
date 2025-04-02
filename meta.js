9dbab1f533a27cc6833a4275204bb7092dcf370c
Vivek Ratan <er.vivekratan@gmail.com>
2025-04-02 14:32:22 +0530

    Create meta.js

---
+++ meta.js
@@ -0,0 +1,37 @@
+window.Epily = {
+    "Epily": {
+        name: "Epily",
+        path: "er.vivekratan/Repositories/Epily",
+        folders: [],
+        files: [
+            { name: "Game.html", type: "html", size: "-", modified: "-", link: "#" },
+            { name: "README.md", type: "md", size: "-", modified: "-", link: "#" },
+            { name: "Winning States", type: "", size: "-", modified: "-", link: "#" },
+            { name: "Without Counter.html", type: "html", size: "-", modified: "-", link: "#" },
+            { name: "oldjs.js", type: "js", size: "-", modified: "-", link: "" },
+            { name: "tic-tac-toe.png", type: "image", size: "-", modified: "-", link: "#" },
+            { name: "tic-tac-toe0.jpg", type: "image", size: "-", modified: "-", link: "#" },
+            { name: "tic-tac-toe1.jpg", type: "image", size: "-", modified: "-", link: "#" },
+            { name: "tic-tac-toe2.jpg", type: "image", size: "-", modified: "-", link: "#" },
+        ]
+    },
+    "D-Tic-Tac-Toe": {
+        name: "D-Tic-Tac-Toe",
+        path: "er.vivekratan/Repositories/Tic-Tac-Toe/D-Tic-Tac-Toe",
+        folders: [],
+        files: [
+            { name: "Game.html", type: "html", size: "-", modified: "-", link: "#" },
+            { name: "README.md", type: "md", size: "-", modified: "-", link: "#" },
+            { name: "T3.css", type: "css", size: "-", modified: "-", link: "#" },
+            { name: "T3.html", type: "html", size: "-", modified: "-", link: "#" },
+            { name: "T3.js", type: "javascript", size: "-", modified: "-", link: "#" },
+            { name: "Winning States", type: "-", size: "-", modified: "-", link: "#" },
+            { name: "Without Counter.html", type: "html", size: "-", modified: "-", link: "#" },
+            { name: "oldjs.js", type: "javascript", size: "-", modified: "-", link: "#" },
+            { name: "tic-tac-toe0.jpg", type: "image", size: "-", modified: "-", link: "#" },
+            { name: "tic-tac-toe1.jpg", type: "image", size: "-", modified: "-", link: "#" },
+            { name: "tic-tac-toe2.jpg", type: "image", size: "-", modified: "-", link: "#" },
+        ]
+    }
+};
+
